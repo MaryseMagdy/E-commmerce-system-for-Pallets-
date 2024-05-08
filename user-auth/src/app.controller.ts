dd512486cb54268e40f6bc6bc7bc62d50d7ea444
MartinaMaurice <126152704+MartinaMaurice@users.noreply.github.com>
2024-05-08 23:24:51 +0300

    change pass bayez, get user info sha8al

--- user-auth/src/app.controller.ts
+++ user-auth/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put } from '@nestjs/common';
 import { userAuthService } from './app.service';
 import { UserDTO } from './dto/create.user.dto'; // Import the UserDTO type
 @Controller('user')
@@ -17,4 +17,14 @@ export class userAuthController {
   async login(@Body() loginDto){
     return this.userAuthService.validateUser(loginDto);
   }
+  @Get('/getuserinfo')
+  async getUserinfo(@Body() userDto){
+    return this.userAuthService.getUserinfo(userDto);
+  }
+  @Put('/changePassword')
+  async changePassword(@Body() userInfoDTO, @Body() newPassword) {
+    return this.userAuthService.changePassword(userInfoDTO, newPassword);
+  }
+
+  
 }
